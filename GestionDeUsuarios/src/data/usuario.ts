f6d034b98390784fad5c575d610ab317ca9bbc94
PabloSantos <psantos-2023251@kinal.edu.gt>
2026-06-30 11:18:47 -0600

    feat: se agregaron datos de prueba

--- GestionDeUsuarios/src/data/usuario.ts
+++ GestionDeUsuarios/src/data/usuario.ts
@@ -1,3 +1,10 @@
 import { BaseUsuario } from '../models/BaseUsuario';
+import { Cliente } from '../models/Cliente';
+import { Administrador } from '../models/Administrador';
 
-export const usuario: BaseUsuario[] = [];
+export const usuario: BaseUsuario[] = [
+  new Cliente(1, 'Carlos Mendoza', 'carlos@mail.com'),
+  new Cliente(2, 'María López', 'maria@mail.com'),
+  new Administrador(3, 'Ana Martínez', 'ana@sistema.com', 'SuperAdmin'),
+  new Administrador(4, 'Pedro Gómez', 'pedro@sistema.com', 'Moderador')
+];
